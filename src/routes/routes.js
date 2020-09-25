6e3fa57f5ca7230e836f194099150934e71e809b
MrGrigoryAlexandrovich <ahmedcvrle@gmail.com>
2020-09-25 19:23:17 +0200

    code sorting and small structure change

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,22 +3,37 @@ const express = require('express');
 const Queries = require('../queries/queries')
 const router = express();
 const queries = new Queries();
-const connection = require('../connection/connection');
 
 //  routes delete
-
 router.delete('/delete/:id',(req,res) => {
-    queries.Delete(req.params.id);
-    res.sendStatus(200);
+    queries.Delete(req.params.id)
+    .then(result => {
+        res.sendStatus(200)
+    })
+    .catch(err=> {
+        res.status(404).end('ID NOT FOUND') 
+    }
+    )
 }); 
+
 //  routes create
 router.post('/create',(req,res) => {
     let URLS = {
     realURL  :req.body.realURL,
     shortURL : queries.unqid(req.protocol+'://'+req.headers.host+'/')
     };
-    queries.Create(URLS.realURL,URLS.shortURL);
-    res.status(200).json(URLS);
+    if(typeof(URLS.realURL) != 'string')
+    res.status(400).end('Field Type must be String')
+    else
+    {
+    queries.Create(URLS.realURL,URLS.shortURL)
+    .then(result => {
+        res.sendStatus(201)
+    })
+    .catch(err=> {
+        res.status(400).end(err) 
+    })
+}
 });
 
 module.exports = router;
